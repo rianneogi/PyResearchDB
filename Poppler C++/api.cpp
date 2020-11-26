b66aa069f769734f1be173db4fa550325ea09f4a
Rian Neogi <rianneogi@gmail.com>
2020-11-26 20:34:46 +0530

    tokenizer

--- Poppler C++/api.cpp
+++ Poppler C++/api.cpp
@@ -1,10 +1,20 @@
 // #include <qt/QtCore>
 #include <QtGui/qimage.h>
 #include <poppler/qt5/poppler-qt5.h>
+#include <boost/tokenizer.hpp>
+#include <iostream>
 
 void run_api()
 {
+	std::string s;
+	std::cin >> s;
 
+	boost::char_separator<char> sep(" ");
+    boost::tokenizer<boost::char_separator<char>> tokens(s, sep);
+	for (boost::tokenizer<boost::char_separator<char>>::iterator beg = tokens.begin(); beg != tokens.end(); ++beg)
+	{
+        std::cout << *beg << "." << std::endl;
+    }
 }
 
 int main()
